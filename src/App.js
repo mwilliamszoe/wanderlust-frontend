49218d738a30203a9e59d3802cc5b5f86fa67bcf
github username <github email address>
2018-11-17 16:33:36 -0600

    added background picture and react router

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import HomepageLayout from "./ClassComponents/HomepageLayout";
+
+class App extends Component {
+  render() {
+    return <HomepageLayout />;
+  }
+}
+
+export default App;
